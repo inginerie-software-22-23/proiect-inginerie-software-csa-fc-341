85dc873f81087b668d92f0351140e74b8db42e4f
crisdijma <cristina.dijmarescu@s.unibuc.ro>
2022-12-07 16:25:15 +0200

    Add Prodanca in Stadioane

--- csa_fc_341/src/Stadion.jsx
+++ csa_fc_341/src/Stadion.jsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import Read_Stadium from './Controlere/Crud_Stadiums/Read_Stadium';
 import Add_Stadium from './Controlere/Crud_Stadiums/Add_Stadium';
+import './Stadion.css';
 export function Stadion() {
   return (
     <div className="main">
 
-        
+  <body>
     <h2 className="main-header">Stadiums List</h2>
 
    
@@ -13,9 +14,8 @@ export function Stadion() {
        <Read_Stadium/>
     
   </div>
-
  
-  
+  </body>  
   </div>
   );
 }
