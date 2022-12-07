d3ad17378e0a192fb253f8ffa31c2a1aff3acb83
Avian Silviu-Gabriel <silviu.avian@gmail.com>
2022-12-07 20:00:55 +0200

    Fixed issue

--- csa_fc_341/src/Stadion.jsx
+++ csa_fc_341/src/Stadion.jsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
 import Read_Stadium from './Controlere/Crud_Stadiums/Read_Stadium';
 import Add_Stadium from './Controlere/Crud_Stadiums/Add_Stadium';
-import './Stadion.css';
 export function Stadion() {
   return (
     <div className="main">
 
-  <body>
+        
     <h2 className="main-header">Stadiums List</h2>
 
    
@@ -14,8 +13,9 @@ export function Stadion() {
        <Read_Stadium/>
     
   </div>
+
  
-  </body>  
+  
   </div>
   );
 }
