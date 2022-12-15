c6d6e8ecd5387cabec31e3e4171ee80c145666e3
crisdijma <cristina.dijmarescu@s.unibuc.ro>
2022-12-15 14:53:44 +0200

    Mini-design Pagina Home

--- csa_fc_341/src/Controlere/Home.js
+++ csa_fc_341/src/Controlere/Home.js
@@ -1,21 +1,41 @@
 import React from "react";
-
+import './Home.css'
 function Home(){
     return (
         <div className="home">
-            <div class="container">
-                <div class="row allign-items-center my-5">
-                    <div class="col-lg-5">
-                        <h2>SQUAD MANAGER</h2>
-                        <h3  class="font-weight-light">Proiect realizat de Ionescu Alexandru, Stan Ana-Maria, Haiducu Stefan-Alexandru, Dijmarescu Cristina, Ioan Tudoar, Avian Silviu</h3>
-                        <p>“Squad Manager” este o aplicatie web pentru gestionarea unui club sportiv. Aceasta este adresata in mod special cluburilor care isi desfasoara activitatea in campionatele de fotbal.</p>
-<p>Aplicatia a fost dezvoltata special pentru a usura munca cluburilor sportive, ajutandu-i pe oamenii ce activeaza in industria sportiva sa gestioneze mai facil clubul, sa aiba acces oricand la toate informatiile echipei.
-</p>
-<p>Squad Manager, dupa cum sugereaza si denumirea, preia din atributiile managerului clubului. Memorand toate datele importante de la nivelul clubului, aplicatia este oriand pregatita sa iti ofere orice informatie ai nevoie referitoare la echipa ta. Cu aplicatia Squad Manager ai un asistent pregatit in permanenta sa raspunda cautarilor tale.</p>
-                    </div>
-                </div>
-            </div>
+
+                        <svg viewBox="0 0 1320 300">
+                        <text x="50%" y="30%" dy=".35em" text-anchor="middle">
+                            SQUAD 
+                        </text>
+                        <text x="50%" y="80%" dy=".35em" text-anchor="middle">
+                            MANAGER
+                        </text>
+                        </svg> 
+                        
+                        <div className="divHome">
+                        <div>
+                            <h1> Despre aplicatie </h1>
+
+                            <p>“Squad Manager” este o aplicatie web pentru gestionarea unui club sportiv. Aceasta este adresata in mod special cluburilor care isi desfasoara activitatea in campionatele de fotbal.</p>
+                            <p>Aplicatia a fost dezvoltata special pentru a usura munca cluburilor sportive, ajutandu-i pe oamenii ce activeaza in industria sportiva sa gestioneze mai facil clubul, sa aiba acces oricand la toate informatiile echipei.</p>
+                            <p>Squad Manager, dupa cum sugereaza si denumirea, preia din atributiile managerului clubului. Memorand toate datele importante de la nivelul clubului, aplicatia este oriand pregatita sa iti ofere orice informatie ai nevoie referitoare la echipa ta. Cu aplicatia Squad Manager ai un asistent pregatit in permanenta sa raspunda cautarilor tale.</p>
+                        </div>
+
+                        <div >
+                            <h1> Echipa developeri </h1>
+                            <ol>
+                            <li><span class="text-wrapper">Avian Silviu</span></li>
+                            <li><span class="text-wrapper">Dijmarescu Cristina</span></li>    
+                            <li><span class="text-wrapper">Haiducu Stefan</span></li>
+                            <li><span class="text-wrapper">Ioan Tudor</span></li>    
+                            <li><span class="text-wrapper">Ionescu Alexandru</span></li>
+                            <li><span class="text-wrapper">Stan Ana</span></li>
+                            </ol>
+                        </div>
+                        </div>
         </div>
+                
     );
 }
 export default Home;
