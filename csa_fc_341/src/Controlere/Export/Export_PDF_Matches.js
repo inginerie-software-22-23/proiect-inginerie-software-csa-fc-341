2004603b8bc702cf63dbeb032d9c49bd362e0ac3
Avian Silviu-Gabriel <silviu.avian@gmail.com>
2023-01-10 19:18:53 +0200

    Integrated Weather API forecast in Match Page

--- csa_fc_341/src/Controlere/Export/Export_PDF_Matches.js
+++ csa_fc_341/src/Controlere/Export/Export_PDF_Matches.js
@@ -5,14 +5,14 @@ import "../Stil.css";
 
 var doc = new jsPDF('l', 'mm', [500, 210]);
 
-doc.text("\tCompetitie\t\t\t\tData\t\tAdversar\t\tArbitru\t\t\tScor", 20, 10);
+doc.text("\tCompetitie\t\t\t\tData\t\tOra\tAdversar\t\tArbitru\t\t\tScor", 20, 10);
 
 
 export function Export_To_PDF(exp_sorted_date_export){
     exp_sorted_date_export.forEach(function(e, i){
         if(20 + (i * 10) > 200){
             doc.addPage();
-            doc.text("\tCompetitie\t\t\t\tData\t\tAdversar\t\tArbitru\t\t\tScor", 20, 10);
+            doc.text("\tCompetitie\t\t\t\tData\t\tOra\tAdversar\t\tArbitru\t\t\tScor", 20, 10);
             i = 0;
         }
         doc.text(
@@ -20,11 +20,13 @@ export function Export_To_PDF(exp_sorted_date_export){
         doc.text(
             e.Data, 100, 20 + (i * 10));
         doc.text(
-            e.Adversar, 143, 20 + (i * 10));
+            e.Ora, 145, 20 + (i * 10));
         doc.text(
-            e.Arbitru, 185, 20 + (i * 10));
+            e.Adversar, 168, 20 + (i * 10));
         doc.text(
-            e.Scor, 250, 20 + (i * 10));
+            e.Arbitru, 210, 20 + (i * 10));
+        doc.text(
+            e.Scor, 272, 20 + (i * 10));
         
     });
     
